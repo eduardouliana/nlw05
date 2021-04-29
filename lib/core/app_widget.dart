62b862336a1377105f17376508c985fe08beae0b
Eduardo Uliana <edu02_rko@hotmail.com>
2021-04-29 19:50:52 -0300

    Alterado para chamar a tela de challenge para teste

--- lib/core/app_widget.dart
+++ lib/core/app_widget.dart
@@ -1,3 +1,4 @@
+import 'package:DevQuiz/challenge/challenge_page.dart';
 import 'package:DevQuiz/home/home_page.dart';
 import 'package:DevQuiz/home/widgets/appbar/app_bar_widget.dart';
 import 'package:DevQuiz/splash/splash_page.dart';
@@ -8,7 +9,7 @@ class AppWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "DevQuiz",
-      home: AppBarWidget(),
+      home: ChallengePage(),
     );
   }
 }
