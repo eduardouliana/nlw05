00b08dec8f5250ed70c401dfe359c82a4a500de4
Eduardo Uliana <edu02_rko@hotmail.com>
2021-04-29 19:49:56 -0300

    Removido padding da barra superior

--- lib/home/widgets/appbar/app_bar_widget.dart
+++ lib/home/widgets/appbar/app_bar_widget.dart
@@ -7,53 +7,50 @@ class AppBarWidget extends PreferredSize {
   AppBarWidget()
       : super(
           preferredSize: Size.fromHeight(250),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Container(
-              height: 250,
-              child: Stack(
-                children: [
-                  Container(
-                    height: 161,
-                    padding: const EdgeInsets.symmetric(horizontal: 20),
-                    width: double.maxFinite,
-                    decoration: BoxDecoration(
-                      gradient: AppGradients.linear,
-                    ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text.rich(
-                          TextSpan(
-                            text: "Olá, ",
-                            style: AppTextStyles.title,
-                            children: [
-                              TextSpan(
-                                  text: "Eduardo",
-                                  style: AppTextStyles.titleBold),
-                            ],
+          child: Container(
+            height: 250,
+            child: Stack(
+              children: [
+                Container(
+                  height: 161,
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    gradient: AppGradients.linear,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text.rich(
+                        TextSpan(
+                          text: "Olá, ",
+                          style: AppTextStyles.title,
+                          children: [
+                            TextSpan(
+                                text: "Eduardo",
+                                style: AppTextStyles.titleBold),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 58,
+                        width: 58,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          image: DecorationImage(
+                            image: NetworkImage(
+                                "https://avatars.githubusercontent.com/u/22794939?v=4"),
                           ),
                         ),
-                        Container(
-                          height: 58,
-                          width: 58,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(10),
-                            image: DecorationImage(
-                              image: NetworkImage(
-                                  "https://avatars.githubusercontent.com/u/22794939?v=4"),
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
+                      )
+                    ],
                   ),
-                  Align(
-                    alignment: Alignment(0.0, 1.0),
-                    child: ScoreCardWidget(),
-                  ),
-                ],
-              ),
+                ),
+                Align(
+                  alignment: Alignment(0.0, 1.0),
+                  child: ScoreCardWidget(),
+                ),
+              ],
             ),
           ),
         );
