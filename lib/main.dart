8f609c4273a23eed0536934133d4f8c21c8b80e9
Eduardo Uliana <edu02_rko@hotmail.com>
2021-04-29 19:51:57 -0300

    removido uses

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:DevQuiz/core/app_widget.dart';
+import 'package:DevQuiz/home/home_page.dart';
 import 'package:flutter/material.dart';
-
-import 'core/app_widget.dart';
 
 void main() {
   runApp(AppWidget());
