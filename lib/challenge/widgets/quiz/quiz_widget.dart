6c1dbfe94adeed7f72c00fbfe8340608fd18e580
Eduardo Uliana <edu02_rko@hotmail.com>
2021-05-17 20:36:08 -0300

    Adicioando widget para quiz

--- lib/challenge/widgets/quiz/quiz_widget.dart
+++ lib/challenge/widgets/quiz/quiz_widget.dart
@@ -1,38 +1,63 @@
 import 'package:DevQuiz/challenge/widgets/answer/answer_widget.dart';
 import 'package:DevQuiz/core/app_text_styles.dart';
+import 'package:DevQuiz/shared/models/answers_model.dart';
+import 'package:DevQuiz/shared/models/question_model.dart';
 import 'package:flutter/material.dart';
 
-class QuizWidget extends StatelessWidget {
-  final String title;
-  const QuizWidget({Key? key, required this.title}) : super(key: key);
+class QuizWidget extends StatefulWidget {
+  final QuestionModel question;
+  final VoidCallback onChange;
+  const QuizWidget({Key? key, required this.question, required this.onChange})
+      : super(key: key);
+
+  @override
+  _QuizWidgetState createState() => _QuizWidgetState();
+}
+
+class _QuizWidgetState extends State<QuizWidget> {
+  int indexSelected = -1;
+
+  AnswerModel answer(int index) => widget.question.anserws[index];
 
   @override
   Widget build(BuildContext context) {
     return Container(
       child: Column(
         children: [
+          SizedBox(
+            height: 64,
+          ),
           Text(
-            title,
+            widget.question.title,
             style: AppTextStyles.heading,
           ),
           SizedBox(
             height: 24,
           ),
-          AnswerWidget(
-            title:
-                "Possibilita a criação de aplicativos compilados nativamente",
-            isRight: true,
-            isSelected: true,
-          ),
-          AnswerWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente"),
-          AnswerWidget(
-              title:
-                  "Possibilita a criação de aplicativos compilados nativamente")
+
+          for (var i = 0; i < widget.question.anserws.length; i++)
+            AnswerWidget(
+              answer: answer(i),
+              disabled: indexSelected != -1,
+              isSelected: indexSelected == i,
+              onTap: () {
+                indexSelected = i;
+
+                setState(() {});
+                Future.delayed(Duration(seconds: 1))
+                    .then((value) => widget.onChange());
+              },
+            )
+
+          /// ... = operador spreed (concatenar a lista)
+          /*...widget.question.anserws
+              .map(
+                (e) => AnswerWidget(
+                  title: e.title,
+                  isRight: e.isRight,
+                ),
+              )
+              .toList(),*/
         ],
       ),
     );
